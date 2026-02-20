826c44d4dbf122a95c68a01991b63abd53dd715a
emilelemoine <lemoine.emile@gmail.com>
2026-02-20 17:04:21 -0500

    Populate utils __init__ with public exports
    Re-export key functions from eeg_helpers and toolkit so users can
    import directly from mosaique.utils.

--- src/mosaique/utils/__init__.py
+++ src/mosaique/utils/__init__.py
@@ -1,2 +1,16 @@
-#!/usr/bin/env ipython
+"""Utility functions for mosaique."""
 
+from mosaique.utils.eeg_helpers import get_event_list, get_region_side
+from mosaique.utils.toolkit import (
+    calculate_over_pool,
+    parallelize_over_axis,
+    save_as_parquet,
+)
+
+__all__ = [
+    "calculate_over_pool",
+    "get_event_list",
+    "get_region_side",
+    "parallelize_over_axis",
+    "save_as_parquet",
+]
